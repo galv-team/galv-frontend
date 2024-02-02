af72dfee587f1b305a5c47cd63d95a2e95ae6cdb
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2024-02-02 12:12:43 +0000

    reduce ErrorPage redundancy

--- src/Components/error/ErrorPage.tsx
+++ src/Components/error/ErrorPage.tsx
@@ -1,10 +1,6 @@
-import Card, {CardProps} from "@mui/material/Card";
-import clsx from "clsx";
-import CardContent from "@mui/material/CardContent";
-import {Link} from "react-router-dom";
-import React, {ReactElement} from "react";
-import CardHeader, {CardHeaderProps} from "@mui/material/CardHeader";
-import useStyles from "../../styles/UseStyles";
+import {CardProps} from "@mui/material/Card";
+import React from "react";
+import ErrorCard, {ErrorCardProps} from "./ErrorCard";
 
 export type ErrorProps = {
     status?: number | null | undefined
@@ -13,29 +9,7 @@ export type ErrorProps = {
     target?: string | number
 }
 
-export type ErrorPageProps = ErrorProps & {header?: ReactElement<CardHeaderProps>}
-
-export default function ErrorPage(props: ErrorPageProps & CardProps) {
-    const {classes} = useStyles();
-    let content = <CardContent>
-        {props.message}
-    </CardContent>
-    if (!props.message) {
-        if (props.status === 401)
-            content = <CardContent>You must <Link to={'/login'}>log in</Link> to see this item.</CardContent>
-        else if (props.status === 403)
-            content = <CardContent>You do not have permission to view this item.</CardContent>
-        else if (props.status === 404)
-            content = <CardContent>This item does not exist.</CardContent>
-        else if (props.status && props.status >= 500)
-            content = <CardContent>Something went wrong.</CardContent>
-        else
-            content = <CardContent>An unknown error occurred.</CardContent>
-    }
-    console.log("ErrorPage", props, content)
-    return <Card className={clsx(classes.itemPage, classes.error)} elevation={0} {...props as CardProps}>
-        {props.header || <CardHeader avatar={"E"} title="Error"/>}
-        {content}
-    </Card>
+export default function ErrorPage(props: ErrorCardProps & CardProps) {
+    return <ErrorCard elevation={0} {...props as CardProps}/>
 
 }
