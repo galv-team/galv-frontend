eaced98b4c365c1354628e96e1ebc8defa60dac7
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2024-07-19 08:55:54 +0100

    fix: raise docs build errors

--- docs/build_docs.py
+++ docs/build_docs.py
@@ -2,6 +2,12 @@ import json
 import os
 import subprocess
 
+def run(*args, **kwargs):
+  """
+  Wrap subprocess.run with default args
+  """
+  return subprocess.run(*args, check=True, shell=True, **kwargs)
+
 # adapted from https://www.codingwiththomas.com/blog/my-sphinx-best-practice-for-a-multiversion-documentation-in-different-languages
 
 # a single build step, which keeps conf.py and versions.yaml at the main branch
@@ -9,16 +15,16 @@ import subprocess
 # and runs the build as we did locally
 def build_doc(tag):
     os.environ["current_version"] = tag
-    subprocess.run("git checkout " + tag, shell=True)
-    subprocess.run("git checkout main -- source/conf.py", shell=True)
-    subprocess.run("git checkout main -- tags.json", shell=True)
-    subprocess.run("make html", shell=True)
+    run("git checkout " + tag
+    run("git checkout main -- source/conf.py"
+    run("git checkout main -- tags.json"
+    run("make html"
 
 # a move dir method because we run multiple builds and bring the html folders to a
 # location which we then push to github pages
 def move_dir(src, dst):
-  subprocess.run(["mkdir", "-p", dst])
-  subprocess.run(f"mv {src}* {dst}", shell=True)
+  run(["mkdir", "-p", dst])
+  run(f"mv {src}* {dst}"
 
 # to separate a single local build from all builds we have a flag, see conf.py
 os.environ["build_all_docs"] = str(True)
