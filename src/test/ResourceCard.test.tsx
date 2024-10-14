7c2d145d0410f6e33c76e4351cd9dadb3ed99566
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2024-10-14 09:42:21 +0100

    Fix docs pipeline (#31)
    * fix: sphinx linting
    * fix: docs don't build under sphinx v8
      - sphinxcontrib.images hasn't updated quickly enough and is using a deprecated alias for status_iterator
    * feat: always try to build docs
    * fix: MacOS 13 test timeouts

--- src/test/ResourceCard.test.tsx
+++ src/test/ResourceCard.test.tsx
@@ -267,7 +267,7 @@ describe('ResourceCard', () => {
             expect(input).toHaveValue('')
             expect(undo_mk2).toBeEnabled()
             check_safe_tooltip_disabled(get_redo_button())
-        }, 10000)
+        }, 500000) // Takes forever on MacOS 13 for some reason?
 
         it('supports resetting', async () => {
             const confirmSpy = vi.spyOn(window, 'confirm')
@@ -976,7 +976,7 @@ describe('ResourceCard', () => {
         //     const row = await setup("key obj", "galv_CELL");
         //     expect(within(row).getByRole('combobox')).toHaveValue("")
         // })
-    })
+    }, 500000) // Takes forever on MacOS 13 for some reason?
 
     describe('ResourceCard shows resume for partial Files', () => {
         it('shows complete upload button for partial files', async () => {
